33ffdb12d9e135b144ca1e4a561d4d2f80063b2a
DanielBenda <dan.benda@gmail.com>
2025-12-23 21:44:21 +0100

    day4 part2

--- days/day04/solution.py
+++ days/day04/solution.py
@@ -3,11 +3,9 @@ from pathlib import Path
 def load_input() -> str:
     return Path(__file__).with_name("input.txt").read_text().strip()
 
-
 def solve():
     data = load_input()
-    lines = data.splitlines()
-    print(lines)
+    lines = [list(line) for line in data.splitlines()]    
     rolls = 0
     for y, line in enumerate(lines):
         for x, ch in enumerate(line):
@@ -16,6 +14,24 @@ def solve():
                     rolls += 1     
     
     print(f"Day 01 - Part 1: {rolls}")
+
+    lines = [list(line) for line in data.splitlines()]    
+    rolls = 0
+    while True:
+        moreRoles = 0
+        for y, line in enumerate(lines):
+            for x, ch in enumerate(line):
+                if ch == '@':
+                    if find_adjacent(lines, x, y):
+                        moreRoles += 1 
+        for y in range(len(lines)):
+            for x in range(len(lines[0])):
+                if lines[y][x] == 'x':
+                    lines[y][x] = '.'
+
+        rolls = rolls + moreRoles
+        if moreRoles == 0:
+            break
 
     print(f"Day 01 - Part 2: {rolls}")
 
@@ -32,8 +48,10 @@ def find_adjacent(grid, x, y):
         ny = y + dy
 
         if 0 <= ny < rows and 0 <= nx < cols:
-            if grid[ny][nx] == '@':
+            if grid[ny][nx] == '@' or grid[ny][nx] == 'x':
                 adjacent_rolls += 1
             pass
+    if adjacent_rolls <= 3:
+        grid[y][x] = 'x'
 
     return adjacent_rolls <= 3
