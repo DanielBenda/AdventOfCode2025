6f9b566c68c0b1c0fab71b09e34cf67eee0010ae
DanielBenda <dan.benda@gmail.com>
2026-01-08 20:53:26 +0100

    day 7 part 2

--- days/day07/solution.py
+++ days/day07/solution.py
@@ -18,8 +18,30 @@ def solve():
                 while beams.__contains__(char):
                     beams.remove(char)
                 counter += 1
-        print(beams)        
     
     print(f"Day 07 - Part 1: {counter}")
+
+    counter = 0
+    width = len(data[0])
+    beams = [0] * width
+
+    for line in data:
+        for char in range(len(line)):
+            if line[char] == 'S':
+                beams[char] = 1
+        new_beams = [0] * width
+    
+        for x in range(width):
+            if beams[x] == 0:
+                continue
+        
+            if line[x] == '^':
+                new_beams[x-1] += beams[x]
+                new_beams[x+1] += beams[x]
+            else:
+                new_beams[x] += beams[x]
+    
+        beams = new_beams
+    counter = sum(beams)
 
     print(f"Day 07 - Part 2: {counter}")
